17a7646397e3ff30c56826cb97391b0d7ee77d94
Alejandro Osorio <wilmer.alejandro.osorio@gmail.com>
2024-03-01 09:41:43 -0500

    Table and colors base

--- src/app/global/service/api.service.ts
+++ src/app/global/service/api.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IArchetype, IMaterial } from '../models/joyaGold.model';
+import { URLS } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +10,18 @@ import { Injectable, inject } from '@angular/core';
 export class ApiService {
 
   private http = inject(HttpClient);
+
+  public getMaterials():Observable<IMaterial>{
+    return  this.http.get<IMaterial>(`${ URLS.BASE }/${URLS.MATERIAL}`);
+  }
+
+  public getArchetypes():Observable<IArchetype>{
+    return  this.http.get<IArchetype>(`${ URLS.BASE }/${URLS.ARCHETYPE}`);
+  }
+
+  /* products */
+
+  public getProducts():Observable<any>{
+    return this.http.get<any>(`${ URLS.BASE }/${URLS.PRODUCT}`);
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {};
+
+export const URLS = {
+    BASE: 'http://localhost:8080/api/v1',
+    PRODUCT: 'product',
+    MATERIAL: 'domain/materials',
+    ARCHETYPE: 'domain/archetypes'
+}
